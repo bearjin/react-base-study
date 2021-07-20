c846a171326903a48079cc780fec809d74be652e
bearjin <kwj5030@gmail.com>
2021-07-21 01:27:34 +0900

    강의 4-5까지 수강 완료

--- NumberBaseball/NuberBaseball.jsx
+++ NumberBaseball/NuberBaseball.jsx
@@ -1,77 +1,77 @@
-import React, { useState, memo } from 'react';
+import React, { useState } from 'react';
 import Try from './Try';
 
 function getNumbers() {
-    const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    const array = [];
-    for (let i = 0; i < 4; i += 1) {
-        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
-        array.push(chosen);
-    }
-    return array;
+  const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const array = [];
+  for (let i = 0; i < 4; i += 1) {
+    const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+    array.push(chosen);
+  }
+  return array;
 }
 
-const NumberBaseball = memo(() => {
-    const [result, setResult] = useState('');
-    const [value, setValue] = useState('');
-    const [answer, setAnswer] = useState(getNumbers());
-    const [tries, setTries] = useState([]);
+const NumberBaseball = () => {
+  const [result, setResult] = useState('');
+  const [value, setValue] = useState('');
+  const [answer, setAnswer] = useState(getNumbers());
+  const [tries, setTries] = useState([]);
 
-    const onSubmitForm = (e) => {
-        e.preventDefault();
-        if (value === answer.join('')) {
-            setResult('홈런');
-            setValue('');
-            setTries((prevTries) => [...prevTries, { try: value, result: '홈런!' }]);
-            alert('게임을 다시 시작합니다.');
-            setValue('');
-            setAnswer(getNumbers());
-            setTries([]);
-        } else {
-            const answerArray = value.split('').map((elm) => parseInt(elm));
-            let strike = 0;
-            let ball = 0;
-            if (tries.length >= 9) {
-                setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')} 였습니다.`);
-                alert('게임을 다시 시작합니다.');
-                setValue('');
-                setAnswer(getNumbers());
-                setTries([]);
-            } else {
-                for (let i = 0; i < 4; i += 1) {
-                    if (answerArray[i] === answer[i]) {
-                        strike += 1;
-                    } else if (answer.includes(answerArray[i])) {
-                        ball += 1;
-                    }
-                }
-                setTries((prevTries) => [...prevTries, { try: value, result: `${strike} 스트라이크 ${ball} 볼` }]);
-                setValue('');
-            }
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (value === answer.join('')) {
+      setResult('홈런');
+      setValue('');
+      setTries((prevTries) => [...prevTries, { try: value, result: '홈런!' }]);
+      alert('게임을 다시 시작합니다.');
+      setValue('');
+      setAnswer(getNumbers());
+      setTries([]);
+    } else {
+      const answerArray = value.split('').map((elm) => parseInt(elm));
+      let strike = 0;
+      let ball = 0;
+      if (tries.length >= 9) {
+        setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')} 였습니다.`);
+        alert('게임을 다시 시작합니다.');
+        setValue('');
+        setAnswer(getNumbers());
+        setTries([]);
+      } else {
+        for (let i = 0; i < 4; i += 1) {
+          if (answerArray[i] === answer[i]) {
+            strike += 1;
+          } else if (answer.includes(answerArray[i])) {
+            ball += 1;
+          }
         }
-    };
+        setTries((prevTries) => [...prevTries, { try: value, result: `${strike} 스트라이크 ${ball} 볼` }]);
+        setValue('');
+      }
+    }
+  };
 
-    const onChageInput = (e) => {
-        e.preventDefault();
-        setValue(e.target.value);
-    };
+  const onChageInput = (e) => {
+    e.preventDefault();
+    setValue(e.target.value);
+  };
 
-    return (
-        <>
-            <h1>{result}</h1>
-            <form onSubmit={onSubmitForm}>
-                <input maxLength={4} value={value} onChange={onChageInput} />
-            </form>
-            <div>시도: {tries.length}</div>
-            <ul>
-                {tries.map((v, i) => {
-                    return (
-                        <Try key={`${i + 1}차 시도:`} tryInfo={v} />
-                    );
-                })}
-            </ul>
-        </>
-    );
-});
+  return (
+    <>
+      <h1>{result}</h1>
+      <form onSubmit={onSubmitForm}>
+        <input maxLength={4} value={value} onChange={onChageInput} />
+      </form>
+      <div>시도: {tries.length}</div>
+      <ul>
+        {tries.map((v, i) => {
+          return (
+            <Try key={`${i + 1}차 시도:`} tryInfo={v} />
+          );
+        })}
+      </ul>
+    </>
+  );
+};
 
 export default NumberBaseball;
--- NumberBaseball/Try.jsx
+++ NumberBaseball/Try.jsx
@@ -1,18 +1,18 @@
 import React, { useState, memo } from 'react';
 
 const Try = memo(({ tryInfo }) => {
-    const [result, setResult] = useState(tryInfo.result);
+  const [result, setResult] = useState(tryInfo.result);
 
-    const onClick = () => {
-        setResult('1');
-    };
+  const onClick = () => {
+    setResult('1');
+  };
 
-    return (
-        <li>
-            <div>{tryInfo.try}</div>
-            <div onClick={onClick}>{tryInfo.result}</div>
-        </li>
-    );
+  return (
+    <li>
+      <div>{tryInfo.try}</div>
+      <div onClick={onClick}>{tryInfo.result}</div>
+    </li>
+  );
 });
 
 export default Try;
